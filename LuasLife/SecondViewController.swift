25c6180a4aaa13641ad3e974ca9991443dd0e405
Peter Lafferty <peadar.lafferty@gmail.com>
2016-02-27 18:42:56 +0000

    Initial Commit

---
+++ LuasLife/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LuasLife
+//
+//  Created by Peter Lafferty on 27/02/2016.
+//  Copyright © 2016 Peter Lafferty. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
