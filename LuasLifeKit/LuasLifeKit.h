0ba86e9c2c3928cdb70666a0edbb54cb5e1da530
Peter Lafferty <peadar.lafferty@gmail.com>
2016-02-29 22:29:37 +0000

    first commit for LuasLifeKit framework

---
+++ LuasLifeKit/LuasLifeKit.h
@@ -0,0 +1,19 @@
+//
+//  LuasLifeKit.h
+//  LuasLifeKit
+//
+//  Created by Peter Lafferty on 29/02/2016.
+//  Copyright © 2016 Peter Lafferty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LuasLifeKit.
+FOUNDATION_EXPORT double LuasLifeKitVersionNumber;
+
+//! Project version string for LuasLifeKit.
+FOUNDATION_EXPORT const unsigned char LuasLifeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LuasLifeKit/PublicHeader.h>
+
+
